ff3c18a35487faa269b8426042621345a54cffa0
FIGUEIREDO alex <figueiredo.alex222@gmail.com>
2023-05-30 16:02:47 +0200

    Restart UE Project

---
+++ Source/FluidMechanic/FluidMechanicGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FluidMechanicGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLUIDMECHANIC_API AFluidMechanicGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
